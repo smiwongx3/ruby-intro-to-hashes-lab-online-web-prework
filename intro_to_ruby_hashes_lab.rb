f263f2ee65e5deeda0e2fcfc7db4c2ece8d0c338
Samantha Wong <smiwong920@gmail.com>
2019-02-03 16:50:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,12 @@ def new_hash
 end
 
 def actor
-  {name: "Dwayne The Rock Johnson"}
+  actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {}
-
+  
 end
 
 def monopoly_with_second_tier
